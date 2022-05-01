b8844246f26c40bc855658cdd48bf927c890be29
Jake Hsu <>
2022-05-01 07:00:40 +0000

    frontend mvp

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MatSelectionListChange } from '@angular/material/list';
+
+type Func = {
+  name: string;
+  code: string;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  backendUrl: string = environment.backendUrl;
+  funcs: Func[] = [
+    {
+      name: 'a',
+      code: "print('a')\n",
+    },
+    {
+      name: 'b',
+      code: "print('b')\n",
+    },
+  ];
+  defaultFunc: Func = {
+    name: 'hello_world',
+    code: `def main(request):\n    return 'Hello, World!'\n`,
+  };
+  selectedFunc: Func | null = null;
+
+  ngOnInit(): void {
+    // this.http
+    //   .get(environment.backendUrl + '/func')
+    //   .subscribe((data) => console.log(data));
+  }
+
+  selectFunc(e: MatSelectionListChange): void {
+    this.selectedFunc = e.options[0].value;
+  }
+
+  addFunc(): void {
+    this.selectedFunc = {
+      name: 'hello_world',
+      code: `def main(request):\n    return 'Hello, World!'\n`,
+    };
+    this.funcs.push(this.selectedFunc);
+  }
+
+  deploy(): void {
+    this.http
+      .put(
+        `${this.backendUrl}/func/${this.selectedFunc.name}`,
+        this.selectedFunc.code
+      )
+      .subscribe((data) => console.log(data));
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
